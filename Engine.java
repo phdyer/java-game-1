22f40aa3e362e969326252300d425ffcb826acb3
Pres <revengetaker@gmail.com>
2014-05-01 08:56:37 -0500

    Engine v2.0
    Changes made to the engine class to reflect the added states. New
    states include menu options, play options, and a controls screen.

--- Engine.java
+++ Engine.java
@@ -6,19 +6,28 @@ import org.newdawn.slick.state.*;
 public class Engine extends StateBasedGame {
 	
 	public static final String gameTitle = "Adventures in Java";
-	public static final int menu0 = 0;
-	public static final int play = 1;
+	public static final int menu = 0;
+	public static final int menuOptions = 1;
+	public static final int playOptions = 2;
+	public static final int controlsScreen = 3;
+	public static final int play = 4;
 	
 	public Engine(String gameTitle) {
 		super(gameTitle);
-		this.addState(new StartMenu(menu0));
+		this.addState(new StartMenu(menu));
+		this.addState(new MenuOptions(menuOptions));
+		this.addState(new PlayOptions(playOptions));
+		this.addState(new ControlsScreen(controlsScreen));
 		this.addState(new Play(play));
 	}
 	
 	public void initStatesList(GameContainer gc) throws SlickException{
-		this.getState(menu0).init(gc, this);
+		this.getState(menu).init(gc, this);
+		this.getState(menuOptions).init(gc, this);
+		this.getState(playOptions).init(gc, this);
+		this.getState(controlsScreen).init(gc,  this);
 		this.getState(play).init(gc, this);
-		this.enterState(menu0);
+		this.enterState(menu);
 	}
 
 	public static void main(String[] args) {
@@ -27,6 +36,7 @@ public class Engine extends StateBasedGame {
 		try {
 			window = new AppGameContainer(new Engine(gameTitle));
 			window.setDisplayMode(700, 500, false);
+			window.setShowFPS(false);
 			window.start();
 		}
 		catch(SlickException e) {
